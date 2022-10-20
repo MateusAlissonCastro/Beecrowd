0af950ec28950953d4f357376c4654ea33267a47
Mateus <mateusalisson15@gmail.com>
2022-10-20 01:32:25 -0300

    Resolução do problema 'Estiagem - 1023' com a linguagem JavaScript (não finalizado)

--- _Estiagem.js
+++ _Estiagem.js
@@ -1,74 +1,84 @@
-const { Console } = require('console');
-
 var input = require('fs').readFileSync('dados.txt', 'utf8');
 var lines = input.split('\n');
 
-//Estrutura para pegar o número de itens correspodente a 
-//quantidade de imóveis - OK
+let lista_valores
 
-//Dividir a quantidade de consumo por quantidade de moradores
-//e arredondarpara baixo - OK
-
-//Organizar os valores de forma crescente indo do menor 
-//consumo por pessoa até o maior
-
-//Dividir a quantidade de aguá consumida pelo número de
-//pessoas em cada cidade
-
-let lista_cons_por_pessoa = []
-
-let pegarValores = () => {
-    let objEst = {}
-    let i_cidade = 1
-    let i_quan_imov = 0
-    let i_imov = 1
-
-    for( let i=0 ; i<lines.length ; i++){
-        if(lines[i].indexOf(' ')===-1 && lines[i]!=='0' ){
-            objEst[`cidade_${i_cidade}`] = {
-                ['quan_imoveis'] : lines[i],
-                ['info_imoveis'] : {},
-            }
-
-            i_quan_imov = i + parseInt(lines[i])
-            i_imov = 1
-            i_cidade++
-
-        }else if( i<=i_quan_imov ){
-            objEst[`cidade_${i_cidade-1}`]['info_imoveis'][`imovel_${i_imov}`] = {
-                [`quan_pessoa`]: parseInt(lines[i].split(' ')[0]),
-                [`cons_agua`]: parseInt(lines[i].split(' ')[1]),
-                consumoPorPessoa() {
-                    return Math.floor(this.cons_agua/this.quan_pessoa)
-                }
-            }
-            i_imov++
-        }else{
-            i = lines.length
+let formatarEntrada = (lista) => {
+    let nova_lista_formatada = []
+    let i = 0
+    while( lista[i]!=='0' ){
+        if( lista[i].indexOf(' ') === -1 ){
+            nova_lista_formatada.push(lista.splice(1+i, parseInt(lista[i])))
         }
+        i++
     }
-    return objEst
+    return nova_lista_formatada
 }
 
-let meu_objeto = pegarValores()
-
-for(chave in meu_objeto.cidade_1.info_imoveis){
-    lista_cons_por_pessoa.push(meu_objeto.cidade_1.info_imoveis[chave])
+let mediaConsumo = (pessoas, imovel) => {
+    return imovel/pessoas
 }
 
-lista_cons_por_pessoa.sort( (a, b) => {
-    if( a.consumoPorPessoa() > b.consumoPorPessoa() )
+let ordenarNumeros = (a, b) => {
+    if( a.n2 > b.n2 ){
         return 1
-    else if( a.consumoPorPessoa() < b.consumoPorPessoa() ){
+    }else if( a.n2 < b.n2 ){
         return -1
     }else{
         return 0
     }
-} )
-let totalP = lista_cons_por_pessoa[0].quan_pessoa + lista_cons_por_pessoa[1].quan_pessoa + lista_cons_por_pessoa[2].quan_pessoa
-let totalC = lista_cons_por_pessoa[0].cons_agua + lista_cons_por_pessoa[1].cons_agua + lista_cons_por_pessoa[2].cons_agua
-let calc = (totalC / totalP).toFixed(2)
+}
 
-console.log(`Cidade# 1
-${lista_cons_por_pessoa[0].quan_pessoa}-${lista_cons_por_pessoa[0].consumoPorPessoa()} ${lista_cons_por_pessoa[1].quan_pessoa}-${lista_cons_por_pessoa[1].consumoPorPessoa()} ${lista_cons_por_pessoa[2].quan_pessoa}-${lista_cons_por_pessoa[2].consumoPorPessoa()}
-Consumo medio: ${calc} m3.`)
+let formatarSaida = (obj) => {
+    let msg = ''
+    for(let i=0 ; i<obj.length; i++){
+        msg += ` ${obj[i].n1}-${obj[i].n2}`
+    }
+    return msg.replace(' ', '')
+}
+
+let comparar_valores = (obj_teste) => {
+    for( let i_prim=0 ; i_prim<obj_teste.length-1 ; i_prim++ ){
+        let item = obj_teste[i_prim].n2
+        for( let i_segu=i_prim+1 ; i_segu<obj_teste.length ; i_segu++ ){
+            if( item === obj_teste[i_segu].n2 ){
+                obj_teste[i_prim].n1 += obj_teste[i_segu].n1
+                obj_teste.splice(i_segu, 1)
+                i_segu-=1
+            }
+        } 
+    }
+    return obj_teste
+}
+
+let main = (lista, indice, superLista) => {
+    let lista_fomatada = []
+    let total_pessoas = 0
+    let total_consumo = 0
+
+    for(let i=0 ; i<lista.length ; i++){
+        let valores = lista[i].split(' ')
+        let media_consumo_imovel = Math.floor(mediaConsumo(parseInt(valores[0]), parseInt(valores[1])))
+        let meuObj = {
+            n1 : parseInt(valores[0]),
+            n2 : media_consumo_imovel
+        }
+
+        total_pessoas += parseFloat(valores[0])
+        total_consumo += parseFloat(valores[1])
+        lista_fomatada.push(meuObj)
+
+        if( i === (lista.length - 1) ){
+            comparar_valores(lista_fomatada)
+        }
+    }
+
+    lista_fomatada.sort( ordenarNumeros )
+
+    console.log(`Cidade# ${indice+1}:\n${formatarSaida(lista_fomatada)}\nConsumo medio: ${(total_consumo/total_pessoas)} m3.${indice !== (superLista.length-1) ? `\n` : ``}`)
+
+    
+}
+
+lista_valores = formatarEntrada(lines)
+lista_valores.forEach(main)
