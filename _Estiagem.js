7f77958fcad02887a02d292c31f5bdf0631beff6
Mateus <mateusalisson15@gmail.com>
2022-10-21 01:42:01 -0300

    Resolução do problema 'Estiagem - 1023' com a linguagem JavaScript

--- _Estiagem.js
+++ _Estiagem.js
@@ -1,20 +1,6 @@
 var input = require('fs').readFileSync('dados.txt', 'utf8');
 var lines = input.split('\n');
 
-let lista_valores
-
-let formatarEntrada = (lista) => {
-    let nova_lista_formatada = []
-    let i = 0
-    while( lista[i]!=='0' ){ //1° repetição
-        if( lista[i].indexOf(' ') === -1 ){
-            nova_lista_formatada.push(lista.splice(1+i, parseInt(lista[i])))
-        }
-        i++
-    }
-    return nova_lista_formatada
-}
-
 let mediaConsumo = (pessoas, imovel) => {
     return imovel/pessoas
 }
@@ -29,7 +15,7 @@ let ordenarNumeros = (a, b) => {
     }
 }
 
-let formatarSaida = (obj) => { //7° repetição
+let formatarSaida = (obj) => {
     let msg = ''
     for(let i=0 ; i<obj.length; i++){
         msg += ` ${obj[i].n1}-${obj[i].n2}`
@@ -37,55 +23,56 @@ let formatarSaida = (obj) => { //7° repetição
     return msg.replace(' ', '')
 }
 
-let comparar_valores = (obj_teste) => {
-    for( let i_prim=0 ; i_prim<obj_teste.length-1 ; i_prim++ ){ //5° repetição
-        let item = obj_teste[i_prim].n2
-        for( let i_segu=i_prim+1 ; i_segu<obj_teste.length ; i_segu++ ){ //6° repetição
-            if( item === obj_teste[i_segu].n2 ){
-                obj_teste[i_prim].n1 += obj_teste[i_segu].n1
-                obj_teste.splice(i_segu, 1)
-                i_segu-=1
-            }
-        } 
-    }
-    return obj_teste
-}
-
 let formatar_casas_decimais = (num, casas) => {
     let ponto = num.indexOf('.')
     return ponto >= 0 ? num.slice(0, ponto+casas+1) : `${num}.${'0'.repeat(casas)}`
 }
 
-let main = (lista, indice, superLista) => {
-    let lista_fomatada = []
+let main = (lista, indice) => {
+    let lista_teste  = []
+    let lista_org = []
     let total_pessoas = 0
     let total_consumo = 0
-    let consumo_medio = 0
+    let consumo_medio
+    let indice_cidade = 1
 
     for(let i=0 ; i<lista.length ; i++){ //3° repetição
-        let valores = lista[i].split(' ')
-        let media_consumo_imovel = Math.floor(mediaConsumo(parseInt(valores[0]), parseInt(valores[1])))
-        let meuObj = {
-            n1 : parseInt(valores[0]),
-            n2 : media_consumo_imovel
-        }
+        let [numero_pessoas, consumo_imovel] = lista[i].split(' '), media_consumo_imovel, indice_valor_existente
 
-        total_pessoas += parseFloat(valores[0])
-        total_consumo += parseFloat(valores[1])
-        lista_fomatada.push(meuObj)
-
-        if( i === (lista.length - 1) ){
-            comparar_valores(lista_fomatada) //4° repetição
+        numero_pessoas = parseInt(numero_pessoas)
+        consumo_imovel = parseInt(consumo_imovel)
+        media_consumo_imovel = Math.floor(mediaConsumo(numero_pessoas, consumo_imovel))
+        indice_valor_existente = lista_teste.indexOf(media_consumo_imovel)
+        total_pessoas += numero_pessoas
+        total_consumo += consumo_imovel
+        
+        
+        if( indice_valor_existente !== -1 ){
+            lista_org[indice_valor_existente].n1 += numero_pessoas
+        }else{
+            lista_teste.push(media_consumo_imovel)
+            lista_org.push({n1:numero_pessoas, n2: media_consumo_imovel})
         }
+        
+       
     }
 
-    lista_fomatada.sort( ordenarNumeros )
+    lista_org.sort(ordenarNumeros)
     consumo_medio = total_consumo/total_pessoas
-
-    console.log(`Cidade# ${indice+1}:\n${formatarSaida(lista_fomatada)}\nConsumo medio: ${formatar_casas_decimais(`${consumo_medio}`, 2)} m3.${indice !== (superLista.length-1) ? `\n` : ``}`)
-
+    consumo_medio = String(consumo_medio)
     
+    console.log(`${indice!==1 ? `\n` : ``}Cidade# ${indice}:\n${formatarSaida(lista_org)}\nConsumo medio: ${formatar_casas_decimais(consumo_medio, 2)} m3.`)
+
 }
 
-lista_valores = formatarEntrada(lines)
-lista_valores.forEach(main) //2° repetição
+let teste = (lista, indice) => {
+    
+    if( lista[0].indexOf(' ') === -1 ){
+        main(lista.splice(1, parseInt(lista[0])), indice)
+        lista.shift()
+    }
+    
+    return lista[0] !== '0' ? teste(lista, indice+1) : indice
+}
+
+teste(lines, 1)
